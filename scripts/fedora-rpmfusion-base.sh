2e27568b2980bfa67211e3e86eb9e818d573925c
Evan Durfee <edurfee@pm.me>
2025-10-11 15:27:15 +0200

    Fix rpm-fusion setup

--- scripts/fedora-rpmfusion-base.sh
+++ scripts/fedora-rpmfusion-base.sh
@@ -6,17 +6,17 @@ set -euo pipefail
 dnf update -y
 
 # Add rpm fusion
-sudo dnf install \
-		https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm \
-		https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm \
-sudo dnf config-manager setopt fedora-cisco-openh264.enabled=1
+dnf install -y \
+	https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm \
+	https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
+dnf config-manager setopt fedora-cisco-openh264.enabled=1
 
 dnf update -y
 
 # Install ffmpeg
-sudo dnf swap ffmpeg-free ffmpeg --allowerasing
-sudo dnf update @multimedia --setopt="install_weak_deps=False" --exclude=PackageKit-gstreamer-plugin
+dnf swap ffmpeg-free ffmpeg --allowerasing -y
+# dnf update @multimedia --setopt="install_weak_deps=False" --exclude=PackageKit-gstreamer-plugin -y
 # Install intel and amd drivers
-sudo dnf install intel-media-driver
-sudo dnf swap mesa-va-drivers mesa-va-drivers-freeworld
-sudo dnf swap mesa-vdpau-drivers mesa-vdpau-drivers-freeworld
+dnf install -y intel-media-driver
+dnf swap mesa-va-drivers mesa-va-drivers-freeworld
+dnf swap mesa-vdpau-drivers mesa-vdpau-drivers-freeworld
