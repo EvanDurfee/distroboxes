448a543f86d156394c90663f7734de5f90f48783
Evan Durfee <edurfee@pm.me>
2025-10-12 12:05:38 +0200

    Take 2 at ADE4.5

--- scripts/adobe-de4.sh
+++ scripts/adobe-de4.sh
@@ -11,8 +11,17 @@ apt update
 apt upgrade -y
 grep -v '^#' ./adobe-de4.packages | xargs apt install -y
 
-export WINEPREFIX=~/.adewine
+export WINEPREFIX=/opt/ade4_pfx
 winetricks --force -q adobe_diged4
+
+cat <<'EOF' | tee /usr/local/bin/ade4.sh
+#!/bin/sh
+
+export WINEPREFIX=/opt/ade4_pfx
+wine "/opt/ade4_pfx/drive_c/Program Files (x86)/Adobe/Adobe Digital Editions 4.5"
+
+EOF
+chmod +x /usr/local/bin/ade4.sh
 
 # Add distrobox shims
 ln -fs /usr/bin/distrobox-host-exec /usr/local/bin/xdg-open
