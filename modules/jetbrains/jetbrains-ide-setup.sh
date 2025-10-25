c4258b1d6bf0148e238e796aa7ef6cc2c22d5eef
Evan Durfee <edurfee@pm.me>
2025-10-25 14:01:48 +0200

    Add desktop icon install to jetbrains installer
    Updates the jetbrains tool installation to copy over the icon file to
    the configured icon directory, as gnome doesn't load the icons when
    specified via absolute path.

--- modules/jetbrains/jetbrains-ide-setup.sh
+++ modules/jetbrains/jetbrains-ide-setup.sh
@@ -21,9 +21,15 @@ _exit () {
 trap '_exit 7' SIGINT SIGTERM SIGHUP
 trap '_exit $?' EXIT
 
-DEFAULT_INSTALL_DIR="${XDG_DATA_HOME:-"${HOME}"/.local/share}/JetBrains/apps"
-DEFAULT_DESKTOP_DIR="${XDG_DATA_HOME:-"${HOME}"/.local/share}/applications"
-DEFAULT_BIN_DIR="${HOME}/.local/bin"
+DEFAULT_USER_INSTALL_DIR="${XDG_DATA_HOME:-"${HOME}"/.local/share}/JetBrains/apps"
+DEFAULT_USER_DESKTOP_DIR="${XDG_DATA_HOME:-"${HOME}"/.local/share}/applications"
+DEFAULT_USER_ICON_DIR="${XDG_DATA_HOME:-"${HOME}"/.local/share}/icons"
+DEFAULT_USER_BIN_DIR="${HOME}/.local/bin"
+
+DEFAULT_SYSTEM_INSTALL_DIR="/opt/JetBrains/apps"
+DEFAULT_SYSTEM_DESKTOP_DIR="/usr/local/share/applications"
+DEFAULT_SYSTEM_ICON_DIR="/usr/local/share/icons"
+DEFAULT_SYSTEM_BIN_DIR="/usr/local/bin"
 
 
 print_help () {
@@ -45,43 +51,57 @@ tools:
   datagrip (DataGrip) for databases
 
 options:
-  -d, --install-dir DIR    directory to install to (default: "$DEFAULT_INSTALL_DIR")
-      --desktop-dir DIR    directory to install desktop file to (default: "$DEFAULT_DESKTOP_DIR")
-      --bin-dir     DIR    directory from which to symlink the editor (default: "$DEFAULT_BIN_DIR")
+      --system             default to system-wide install directories
+      --user               default to user install directories
       --no-install         skip tool installation
-      --no-desktop         skip desktop (launcher) installation
+      --only-install       only install the tool, skip desktop, icon, and bin setup
+      --no-desktop         skip desktop (launcher) and icon installation
       --no-bin             skip adding symlink to bin dir
   -h, --help               print usage information and exit
 
+For fine-grained control, the exact directory for each component can be
+specified.
+
+install dirs:
+  -d, --install-dir DIR    directory to install to (default: "$DEFAULT_USER_INSTALL_DIR")
+      --desktop-dir DIR    directory to install desktop file to (default: "$DEFAULT_USER_DESKTOP_DIR")
+      --icon-dir    DIR    directory to install desktop icon file to (default: "$DEFAULT_USER_ICON_DIR")
+      --bin-dir     DIR    directory from which to symlink the editor (default: "$DEFAULT_USER_BIN_DIR")
+
 examples:
 
-User install with no bin added to "${DEFAULT_BIN_DIR}":
-  $PROG --no-bin pycharm goland
+User install with no bin added to "${DEFAULT_USER_BIN_DIR}":
+  $PROG --user --no-bin pycharm goland
 
-System install:
-  $PROG --install-dir=/opt/Jetbrains/apps \\
-          --desktop-dir=/usr/local/share/applications \\
-          --bin-dir=/usr/local/bin \\
-          pycharm goland
+System install to with custom data dir:
+  $PROG --system --install-dir=/var/opt/Jetbrains/apps idea
 EOF
 }
 
 main() {
 	# Requires gnu enhanced getopt
-	ARGS=$(getopt --name "$PROG" --long 'help,install-dir:,desktop-dir:,bin-dir:,no-desktop,no-install,no-bin' --options 'hd:' -- "$@")
+	ARGS=$(getopt --name "$PROG" --long 'help,user,system,install-dir:,desktop-dir:,icon-dir:,bin-dir:,no-install,only-install,no-desktop,no-bin' --options 'hd:' -- "$@")
 	eval set -- "$ARGS"
 
-	install_dir="$DEFAULT_INSTALL_DIR"
-	desktop_dir="$DEFAULT_DESKTOP_DIR"
-	bin_dir="$DEFAULT_BIN_DIR"
-	do_add_desktop_entry=true
+	install_dir=""
+	desktop_dir=""
+	icon_dir=""
+	bin_dir=""
+	default_to_user=true
 	do_install=true
+	do_add_desktop_entry=true
 	do_link_bin=true
 	while [ $# -gt 0 ]; do
 		case "$1" in
 			-h | --help)
 				print_help
 				exit 0
+				;;
+			--user)
+				default_to_user=true
+				;;
+			--system)
+				default_to_user=false
 				;;
 			-d | --install-dir)
 				shift
@@ -91,15 +111,24 @@ main() {
 				shift
 				desktop_dir="$1"
 				;;
+			--icon-dir)
+				shift
+				icon_dir="$1"
+				;;
 			--bin-dir)
 				shift
 				bin_dir="$1"
 				;;
-			--no-desktop)
-				do_add_desktop_entry=false
-				;;
 			--no-install)
 				do_install=false
+				;;
+			--only-install)
+				do_install=true
+				do_add_desktop_entry=false
+				do_link_bin=false
+				;;
+			--no-desktop)
+				do_add_desktop_entry=false
 				;;
 			--no-bin)
 				do_link_bin=false
@@ -111,6 +140,19 @@ main() {
 		esac
 		shift
 	done
+
+	if [ -z "$install_dir" ]; then
+		if $default_to_user; then install_dir="$DEFAULT_USER_INSTALL_DIR"; else install_dir="$DEFAULT_SYSTEM_INSTALL_DIR"; fi
+	fi
+	if [ -z "$desktop_dir" ]; then
+		if $default_to_user; then desktop_dir="$DEFAULT_USER_DESKTOP_DIR"; else desktop_dir="$DEFAULT_SYSTEM_DESKTOP_DIR"; fi
+	fi
+	if [ -z "$icon_dir" ]; then
+		if $default_to_user; then icon_dir="$DEFAULT_USER_ICON_DIR"; else icon_dir="$DEFAULT_SYSTEM_ICON_DIR"; fi
+	fi
+	if [ -z "$bin_dir" ]; then
+		if $default_to_user; then bin_dir="$DEFAULT_USER_BIN_DIR"; else bin_dir="$DEFAULT_SYSTEM_BIN_DIR"; fi
+	fi
 
 	if [ $# -lt 1 ]; then
 		echo "No tool name specified" >&2
@@ -176,7 +218,7 @@ main() {
 			install_jetbrains_ide "$install_dir" "$tool_name" "$binary_name" "$tool_code" "$arch"
 		fi
 		if $do_add_desktop_entry; then
-			add_desktop_entry "$desktop_dir"  "$install_dir" "$tool_name" "$binary_name"
+			add_desktop_entry "$desktop_dir" "$icon_dir" "$install_dir" "$tool_name" "$binary_name"
 		fi
 		if $do_link_bin; then
 			ln --symbolic --force --no-target-directory "$install_dir"/"$binary_name"/bin/"$binary_name" "$bin_dir"/"$binary_name"
@@ -187,21 +229,30 @@ main() {
 
 add_desktop_entry() {
 	desktop_dir="$1"; shift
+	icon_dir="$1"; shift
 	install_dir="$1"; shift
 	tool_name="$1"; shift
 	binary_name="$1"; shift
 
+	# copy desktop icon
+	mkdir -p "$icon_dir"
+	# In theory, we want scalable icons in $prefix/share/icons/hicolor/scalable/app/<app-name>.svg, but I was unable to get those to be used (perhaps because the application has no gtk application_id?).
+	# Best we can do is rely on the DE supporting .svg and reading it from the icon dir root (Gnome 49 does this, at least)
+	cp "${install_dir}"/"${binary_name}"/bin/"${binary_name}".svg --target-directory "$icon_dir"
+
+	# create desktop launcher
 	cat <<EOF | tee "$desktop_dir"/"$binary_name".desktop >/dev/null
 [Desktop Entry]
-Name="${tool_name}"
+Name=${tool_name}
 Exec="${install_dir}/${binary_name}/bin/${binary_name}" %u
 Version=1.0
 Type=Application
 Categories=Development;IDE;
 Terminal=false
-Icon="${install_dir}/${binary_name}/bin/${binary_name}.svg"
-Comment="JetBrains ${tool_name} IDE"
-StartupWMClass="jetbrains-${binary_name}"
+# Icon=${install_dir}/${binary_name}/bin/${binary_name}.svg
+Icon=${binary_name}
+Comment=JetBrains ${tool_name} IDE
+StartupWMClass=jetbrains-${binary_name}
 StartupNotify=true
 EOF
 }
